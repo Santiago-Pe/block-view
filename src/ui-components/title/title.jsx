96fd120573a3252dbc7f6ec0eb13d6e034887afa
Santiago-pe <penasantiago346@gmail.com>
2024-03-21 19:43:19 -0300

    EDIT: edit title component to handel spacing

--- src/ui-components/title/title.jsx
+++ src/ui-components/title/title.jsx
@@ -1,14 +1,15 @@
 import PropTypes from "prop-types";
 
-const Title = ({ text, level }) => {
+const Title = ({ text, level, align = "center" }) => {
   const Heading = `h${level}`;
 
-  return <Heading className="my-3">{text}</Heading>;
+  return <Heading className={`my-3 text-${align}`}>{text}</Heading>;
 };
 
 Title.propTypes = {
   text: PropTypes.string.isRequired,
   level: PropTypes.oneOf([1, 2, 3, 4, 5, 6]).isRequired,
+  align: PropTypes.string,
 };
 
 export default Title;

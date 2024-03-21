d8ba3fd02b02ce843cf1c9b3ed6f93a468d9c57f
Santiago-pe <penasantiago346@gmail.com>
2024-03-21 19:42:53 -0300

    ADD: create new ui components

--- src/ui-components/navbar/navbar.jsx
+++ src/ui-components/navbar/navbar.jsx
@@ -1,12 +1,17 @@
 import { Link } from "react-router-dom";
 import Button from "../button/button";
+import BreadCrumbs from "../breadcrumbs/breadcrumbs";
+import { useContext } from "react";
+import { BreadCrumbContext } from "../../context/breadcrumbContext";
 
 const Navbar = () => {
+  const { breadcrumbs } = useContext(BreadCrumbContext);
   const conectWallet = () => {
     console.log("connect metamask");
   };
   return (
     <nav className="d-flex justify-content-end align-items-center">
+      <BreadCrumbs crumbs={breadcrumbs} />
       <div className="me-4">
         <Link to={"/"}>Home</Link>
       </div>
